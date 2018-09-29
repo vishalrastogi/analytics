97e0f09e42d279c8fc05a99877e384ad27744fe0
vishalrastogi <vishal1292@gmail.com>
2018-09-30 00:19:11 +0530

    day1

---
+++ DS.R
@@ -0,0 +1,293 @@
+#Data Structure in R
+
+#Vectors----
+x=1:10    #create seq of nos. for 1 to 10
+x
+x1 <- 1:20
+x1
+
+(x1=1:30)     #assign and print altogether
+(x2=c(1,2,13,4,5))
+class(x2)
+class(x)
+(x3=letters[1:10])
+class(x3)
+LETTERS[1:26]
+(x3b = c('a',"Vishal","4"))
+?c
+
+(x4=c(T,FALSE,TRUE,T,F))
+class(x4)
+
+x5=c(3L,5L)
+class(x5)
+x5a=c(3,5)
+class(x5a)
+(x5b=c(1,'a',T,4L))
+class(x5b)
+
+#access elements
+(x6 = seq(0,100,by=3))
+methods(class='numeric')
+ls()    #variables in environment
+length(x6)
+x6[20]
+x6[3]
+x6[c(2,4)]
+x6[-1]    # all but 1st element
+x6[-c(1:10,15:20)]
+x6[c(2.56,13.52)]
+x6[-(length(x6)-1)]
+#can not mix positive and negative
+(x7 = c(x6,x2))
+
+
+
+#modify
+sort(x6)
+sort(x6 , decreasing = T)
+sort(x7 , decreasing = T)
+rev(x7)
+
+
+seq(-3,10,by=0.2)
+(x=-13:2)
+x[2] <- 0;x
+x < -5
+
+x[x < -5] = 5;x    #modify elements less than -5
+
+x = x[1:4];x
+
+#delete vector
+(x=seq(1,5, length.out = 10))
+x= NULL
+x
+
+(x = rnorm(100))
+?rnorm
+plot(density(x))
+(x1 = rnorm(100000, mean=50, sd=5))
+plot(density(x1))
+abline(v=mean(x1),h=0.04)    #plotting a line in graph
+
+
+#Matrices----
+1:12
+(m1 = matrix(1:12, nrow = 4))
+(m2 = matrix(1:12, ncol = 3 , byrow = T))
+x=101:124
+matrix(x, ncol=6)
+
+class(m1)
+attributes(m1)
+dim(m1)
+
+#access elements of matrix
+m1[1,2:3]
+m1[c(1,3),]
+
+paste("C","D",sep = "-")
+paste("C",1:100,sep = "-")
+
+(colnames(m1) = paste("C",1:3,sep = "-"))
+m1
+(rownames(m1) = paste("R",1:4,sep = "-"))
+m1
+attributes(m1)
+
+#vector to matrix
+(m3=1:24)
+dim(m3)=c(6,4)
+m3
+
+m2
+m2[1:5]
+m2[c(T,F,T,F), c(2,3)]
+m2
+
+
+rbind(m2,c(50,60,70))
+cbind(m2,c(50,60,70,80))
+
+colSums(m1);rowSums(m1)
+colMeans(m1);rowMeans(m1)
+
+t(m1) # transpose
+m1
+sweep(m1, MARGIN = 1, STATS = c(2,3,4,5), FUN="+") #row wise
+m1
+sweep(m1, MARGIN = 2, STATS = c(2,3,4), FUN="*") #col wise
+?sweep
+
+#addmargins
+addmargins(m1,margin = 1,sum) #colwise
+addmargins(m1,margin = 1,sd)
+
+addmargins(m1,margin = 2,sum) #rowwise
+addmargins(m1,margin = 2,mean)
+
+addmargins(m1,c(1,2),list(list(mean,sum,max),list(var,sd)))
+
+#Arrays----
+#Array-----
+length(100:123)
+4*3*2
+#2 coys, 3 products, 4 locations sold qty
+(a1 = array(100:123, dim=c(4,3,2)))
+(loc = paste('loc', 1:4,sep='-'))
+(product = paste('p', 1:3,sep='@'))
+(coy = paste('coy', 1:2,sep='%'))
+dimnames(a1) = list(loc, product, coy)
+a1
+apply(a1,1, sum) #locationwise
+apply(a1,2, sum) #productwise
+apply(a1,c(1,2), sum) #product-location wise
+apply(a1,c(2,3), sum) #product-coy wise
+apply(a1,c(1,3), sum) #coy-location
+apply(a1,3, sum) #coywise
+sum(a1) #total
+
+
+#Data frames----
+(rollno = 1:30)
+(sname = paste('student',1:30,sep=''))
+set.seed(1234)
+(gender = sample(c('M','F'), size=30, replace=T, prob=c(.7,.3)))
+table(gender)
+prop.table(table(gender))
+
+(marks = floor(rnorm(30,mean= 50,sd=10)))
+(marks2 = ceiling(rnorm(30,40,5)))
+(course = sample(c('BBA','MBA'), size=30, replace=T, prob=c(.5,.5)))
+rollno; sname; gender
+marks ; marks2; course
+
+?floor
+
+
+#create DF
+df1= data.frame(rollno, sname, gender, marks, marks2, course, stringsAsFactors = F)
+df1
+str(df1) #structure of DF
+head(df1) #top 6 rows
+head(df1,n=3) #top 3 rows
+tail(df1) #last 6 rows
+class(df1) # DF
+summary(df1) #summary
+df1$gender = factor(df1$gender)
+df1$course = factor(df1$course)
+str(df1)
+summary(df1)
+
+df1  #full data
+df1$gender  # one column
+df1[ , c(2,4)] #multiple columns
+df1[1:10 ,] #select rows, all columns
+#as per conditionis
+df1[ marks > 50 & gender=='F', c('rollno', 'sname','gender', 'marks')]
+df1[ marks > 50 & gender=='F', c(1,2)]
+df1[ marks > 50 | gender=='F', ]
+
+names(df1)  # names of columns
+dim(df1)  #Dimensions
+
+aggregate(df1$marks, by=list(df1$gender), FUN=max)
+aggregate(marks ~ gender, data=df1, FUN=max)
+
+
+(df2 = aggregate(cbind(marks,marks2) ~ gender + course, data=df1, FUN=mean))
+
+
+#List -----
+g ="My First List"
+h = c(25, 26,18,39)
+j = matrix(1:10,nrow=2)
+k = c('one','two','three')
+mylist = list(title=g, ages=h, j, h)
+mylist
+mylist[2]
+mylist[[2]]
+mylist[['ages']]
+mylist$ages
+
+
+
+
+
+#Factor -----
+
+(grades = sample(c('A','B','C','D'), size=30, replace=T, prob=c(.3,.2,.4,.1)))
+summary(grades)
+table(grades)
+(gradesFactor = factor(grades))
+summary(gradesFactor)
+
+(gradesFactorOrdered = factor(grades, ordered=T))
+summary(gradesFactorOrdered)
+
+(gradesFactorOrderedLevels = factor(grades, ordered=T, levels=c('D','C','B','A')))
+summary(gradesFactorOrderedLevels)
+
+pie(summary(gradesFactorOrderedLevels))
+barplot(summary(gradesFactorOrderedLevels))
+
+class(grades)
+class(gradesFactorOrdered)
+class(gradesFactorOrderedLevels)
+
+
+
+# Object Properties
+#vector
+v1= 1:100
+class(v1) ; typeof(v1)
+v2=letters[1:10]
+class(v2) ; typeof(v2)
+length(v2)
+summary(v1)
+
+#matrix
+m1= matrix(1:24,nrow=6)
+class(m1)
+summary(m1)
+dim(m1)
+str(m1)
+
+#Array
+a1 =array(1:24, dim=c(4,3,2))
+class(a1)
+str(a1)
+dim(a1)
+summary(a1)
+
+
+
+#DF
+#data() #built in datasets
+df1= iris 
+str(df1)
+summary(df1)
+class(df1); dim(df1)
+nrow(df1) ; names(df1) ;NROW(df1)
+colnames(df1)
+rownames(df1)
+
+#list
+list1 = list(v1,m1,a1,df1)
+str(list1)
+
+#Statistical Description
+library(Hmisc)
+describe(df1)
+
+
+#lets draw some simple graphs
+
+
+
+
+#List----
+
+#Factor----
+
